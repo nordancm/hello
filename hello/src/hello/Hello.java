f619c3107bc76918d2594dca050af0512193f8e2
nordancm <nordancm@CS-ABH309-23.appd.appstate.edu>
2016-01-27 13:25:23 -0500

    Added structure for name input, can handle no or various options

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -4,7 +4,22 @@ public class Hello {
 
 	public static void main(String [ ] args)
 	{
-	     System.out.println("Hello World!!");
-	     System.out.println("Hey there!!");
+		if(args[1] == null)
+		{
+			System.out.println("Hello World!!");
+		}
+		else
+		{
+			if(args[2] == null)
+			{
+				System.out.println("Hello " + args[1] + '!');
+			}
+			else
+			{
+				System.out.println("Hello " + args[1] + ". My name is " 
+				+ args[2] + ". Hope you are doing well.");
+			}
+		}
+	   
 	}
 }
